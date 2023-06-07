b787390fe39f605940d8e3000fbbe588cc1cc55a
SuarezRocio <suarez.rocio4344@gmail.com>
2023-06-07 17:46:22 -0300

    changes on database

--- src/database/BaseDatabase.ts
+++ src/database/BaseDatabase.ts
@@ -1,14 +1,18 @@
 import { knex } from "knex"
 import dotenv from 'dotenv'
+import * as path from 'path'
 
 dotenv.config()
 
+console.log(path.join(__dirname, '../../labeedit.db'))
 export abstract class BaseDatabase {
     protected static connection = knex({
         client: "sqlite3",
         connection: {
-            /*  filename: process.env.DB_FILE_PATH as string */
-            filename: "./src/database/labeedit.db",
+            /*filename: process.env.DB_FILE_PATH as string
+            /*filename: "./src/database/labeedit.db",
+            filename: "/labeedit.db"*/
+            filename: path.join(__dirname, '../../labeedit.db')
         },
         useNullAsDefault: true,
         pool: {
